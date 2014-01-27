0af72074618fe509ec40e4efa2df775851f692dd
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2014-01-27 02:08:08 +0100

    Working & updated examples of Makefiles

--- make/launch_echo_2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-hadoop fs -rmr make-echos ; hadoop fs -copyFromLocal ../Makefiles/echos/ make-echos ; mvn clean install jar:jar
-cd ../Makefiles/make-echos
-hadoop jar ../make/make-0.0.1-SNAPSHOT.jar hadoop_playground.make.Make make-echos all.txt
-echo "OUTPUT:"
-cat all.txt
--- make/launch_echos.sh
+++ make/launch_echos.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 hadoop fs -rmr make-echos ; hadoop fs -copyFromLocal ../Makefiles/echos/ make-echos ; mvn clean install jar:jar
-cd make-echos
-hadoop jar ../make-0.0.1-SNAPSHOT.jar hadoop_playground.make.Make . all.txt
+hadoop jar make-0.0.1-SNAPSHOT.jar hadoop_playground.make.Make make-echos all.txt
 echo "OUTPUT:"
-cat all.txt
+hadoop fs -cat make-echos/all.txt
